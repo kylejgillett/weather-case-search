adab3ebfe062c941f7b6423c8241288574a1f581
Jim Tang <wxfreakk91@gmail.com>
2025-01-05 20:46:56 -0600

    formatting

--- weather_cases/soundings/rapruc.py
+++ weather_cases/soundings/rapruc.py
@@ -27,26 +27,28 @@ def rapruc_sounding(dt: DateTimeLike, lat: float, lon: float):
     with warnings.catch_warnings(action="ignore"):
 
         # load dictionary of profile data from SounderPy
-        data_dict = spy.get_model_data('rap-ruc', [lat, lon], year_str, month_str, day_str, hour_str)
+        data_dict = spy.get_model_data(
+            "rap-ruc", [lat, lon], year_str, month_str, day_str, hour_str
+        )
 
         # parse wind speed & direction from u and v
-        wind_speed = mp.wind_speed(data_dict['u'], data_dict['v'])
-        wind_direction = mp.wind_direction(data_dict['u'], data_dict['v'])
+        wind_speed = mp.wind_speed(data_dict["u"], data_dict["v"])
+        wind_direction = mp.wind_direction(data_dict["u"], data_dict["v"])
 
         profile_data = ProfileData(
-            level=nan_to_none(data_dict['p'].m),
-            hght=nan_to_none(data_dict['z'].m),
-            temp=nan_to_none(data_dict['T'].m),
-            dwpt=nan_to_none(data_dict['Td'].m),
+            level=nan_to_none(data_dict["p"].m),
+            hght=nan_to_none(data_dict["z"].m),
+            temp=nan_to_none(data_dict["T"].m),
+            dwpt=nan_to_none(data_dict["Td"].m),
             wspd=nan_to_none(wind_speed.m),  # type: ignore
             wdir=nan_to_none(wind_direction.m),  # type: ignore
-            omega=nan_to_none(data_dict['omega'].m),
+            omega=nan_to_none(data_dict["omega"].m),
         )
 
         return Profile(
             lat=lat,
             lon=lon,
             timestamp=pd.Timestamp(dt).to_pydatetime(),
-            source=data_dict['site_info']['model'],
+            source=data_dict["site_info"]["model"],
             data=profile_data,
         )
